58e18060308e7127a7c98c4b544cbe02d4dc4656
Alessandro Marvão Nascimento <6375939-alessandromarvao@users.noreply.gitlab.com>
2022-03-06 15:44:45 -0300

    Criando pagina Home

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import React from 'react';
 import './style.css';
 import PageRoutes from './pageRoutes';
 
-
-// https://sujeitoprogramador.com/r-api/?api=filmes
-
 export default function App() {
 	return (
 		<div className='app'>
